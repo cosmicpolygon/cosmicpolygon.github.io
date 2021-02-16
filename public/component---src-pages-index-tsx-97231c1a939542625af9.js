0a253836dcf061716472ed90236d9ac6573168e9
Chris Peters <bildepunkt@gmail.com>
2021-02-15 20:58:55 -0800

    1613451535

---
+++ public/component---src-pages-index-tsx-97231c1a939542625af9.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{QeBL:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),o=t.n(a),c=t("9Dj+"),i=t("H8eV");n.default=function(){return o.a.createElement(c.a,null,o.a.createElement(i.a,{title:"Cosmic Polygon"}))}}}]);
+//# sourceMappingURL=component---src-pages-index-tsx-97231c1a939542625af9.js.map
