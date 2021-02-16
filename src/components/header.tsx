a3d4be65d544c2ae280cefef8a84ee980145357c
Chris Peters <bildepunkt@gmail.com>
2021-02-15 19:30:13 -0800

    update header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,10 +7,8 @@ import * as styles from './header.module.scss';
 
 const Header = () => (
   <header className={styles.container} style={{ height: useCurrentHeight() }}>
-    <div className={styles.hero}>
-      <div className={styles.logo}></div>
-      <Link className={styles.title} to="/">COSMIC POLYGON</Link>
-    </div>
+    <div className={styles.logo}></div>
+    <Link className={styles.title} to="/">COSMIC POLYGON</Link>
   </header>
 );
 
