6972e278c5a1c7615a28fbdbec0d06982a47b3d3
Chris Peters <bildepunkt@gmail.com>
2021-02-15 19:17:15 -0800

    update header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,21 +6,6 @@ import { useCurrentHeight } from "./hooks";
 import * as styles from './header.module.scss';
 
 const Header = () => {
-  let height;
-
-  useEffect(() => {
-    const resize = () => {
-      height = window.innerHeight;
-    };
-
-    resize();
-    window.addEventListener('resize', resize);
-
-    return () => {
-      window.removeEventListener('resize', resize);
-    }
-  }, [])
-
   return (
     <header className={styles.container} style={{ height: useCurrentHeight() }}>
       <div className={styles.hero}>
