488631ec7853cb6136d2b0021bd164b50ac75e37
Chris Peters <bildepunkt@gmail.com>
2021-02-21 11:35:42 -0800

    comment out mobile menu, remove bg color from header on home

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,12 +7,12 @@ import Icon from '../images/logo.inline.svg';
 import * as styles from './header.module.scss';
 
 const Header = ({ page }) => (
-  <header className={styles.container}>
+  <header className={`${styles.container} ${page === pages.HOME ? styles.isHome : ''}`}>
     <nav>
       <div className={styles.col}>
         <Link
           to="/"
-          className={`${styles.home} ${page === pages.HOME ? styles.isHome : ''}`}
+          className={styles.home}
           title="Home">
           <Icon className="headerHomeIcon" />
         </Link>
@@ -22,7 +22,9 @@ const Header = ({ page }) => (
         <Link to="/about">ABOUT ME</Link>
       </div>
     </nav>
-    <div className={styles.mobileMenuButton}>menu</div>
+    <div className={styles.mobileMenuButton}>
+      <button>menu</button>
+    </div>
   </header>
 );
 
