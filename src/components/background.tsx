e8a248415a699f97dfe466aa836f595dc5e6a2f3
Chris Peters <bildepunkt@gmail.com>
2021-02-21 11:24:28 -0800

    organize home page modules, add background

---
+++ src/components/background.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import * as styles from './background.module.scss';
+
+const Background = ({ width, height }): JSX.Element => (
+  <div className={styles.container} />
+);
+
+export default Background;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-import Home from "../components/home";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-
+import Logo from "../components/logo";
+import Background from "../components/background";
 import { pages } from "../common/types";
 
-const IndexPage = () => (
-  <Layout page={pages.HOME}>
-    <SEO title="Design, UX, & Illustration" />
-    <Home />
-  </Layout>
-);
+import styles from '../common/index.module.scss';
+
+const IndexPage = (): JSX.Element => {
+  const [height, setHeight] = useState(typeof window !== 'undefined' ? window.innerHeight : undefined);
+  const [width, setWidth] = useState(typeof window !== 'undefined' ? window.innerWidth : undefined);
+
+  useEffect(() => {
+    let timeoutId = null;
+    const handleResize = () => {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+        setHeight(window.innerHeight);
+        setWidth(window.innerWidth);
+      }, 256);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    }
+  }, []);
+
+  return (
+    <Layout page={pages.HOME}>
+      <SEO title="Design, UX, & Illustration" />
+      <div className={styles.container} style={{ height }}>
+        {/* <Background width={width} height={height} /> */}
+        <Logo />
+        <div className={styles.title}>
+          <h1>COSMIC POLYGON</h1>
+          <h3>DESIGN, UX, & ILLUSTRATION</h3>
+        </div>
+      </div>
+    </Layout>
+  )
+};
 
 export default IndexPage;
