25dadca6fa7c9776a94bd085c3f7d37934986132
Chris Peters <bildepunkt@gmail.com>
2021-06-11 21:06:55 -0700

    convery home link to text

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from "gatsby";
 
 import { pages } from "../common/types";
-import Icon from '../images/logo.inline.svg';
 
 import * as styles from './header.module.scss';
 
@@ -10,12 +9,7 @@ const Header = ({ page }) => (
   <header className={`${styles.container} ${page === pages.HOME ? styles.isHome : ''}`}>
     <nav>
       <div className={styles.col}>
-        <Link
-          to="/"
-          className={styles.home}
-          title="Home">
-          <Icon className="headerHomeIcon" />
-        </Link>
+        <Link className={styles.home} to="/">HOME</Link>
       </div>
       <div className={styles.col}>
         <Link to="/projects">PROJECTS</Link>
